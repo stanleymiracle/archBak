3c0f30a277b5f15957ede7097130cc49a6cb0b93
Stanley Miracle <logicomiracle@gmail.com>
2015-06-21 13:51:36 -0700

    UPDATE

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ cp ~/.xinitrc ~/repo/archBak/config/shell/.xinitrc
 cp ~/.Xresources ~/repo/archBak/config/shell/.Xresources
 cp ~/.bash_profile ~/repo/archBak/config/shell/.bash_profile
 cp ~/repo/8812au.sh ~/repo/archBak/config/shell/8812au.sh
-cp ~/.emacs.d/init.el ~/repo/archBak/config/emacs/init.el
+cp ~/.emacs.d/init.el ~/repo/archBak/config/emacs/.emacs.d/init.el
 cp ~/.mplayer/config ~/repo/archBak/config/mplayer/config
 cp /etc/vimrc ~/repo/archBak/config/vim/vimrc
 cp ~/.xpdfrc ~/repo/archBak/config/xpdf/.xpdfrc
