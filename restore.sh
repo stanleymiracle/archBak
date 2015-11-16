bf3fc1eea9bfcb3a967a5832f5465eb5e3985311
Stanley Miracle <logicomiracle@gmail.com>
2015-11-16 01:43:01 -0800

    UPDATE

---
+++ restore.sh
@@ -0,0 +1,13 @@
+cp ~/repo/archBak/config/awesome/rc.lua          ~/.config/awesome/rc.lua
+cp ~/repo/archBak/config/awesome/sound.sh        ~/.config/awesome/sound.sh
+cp ~/repo/archBak/config/awesome/ones.sh         ~/.config/awesome/ones.sh
+cp ~/repo/archBak/config/awesome/dual.sh         ~/.config/awesome/dual.sh 
+cp ~/repo/archBak/config/shell/.bashrc           ~/.bashrc
+cp ~/repo/archBak/config/shell/.xinitrc          ~/.xinitrc
+cp ~/repo/archBak/config/shell/.Xresources       ~/.Xresources 
+cp ~/repo/archBak/config/shell/.bash_profile     ~/.bash_profile
+cp ~/repo/archBak/config/emacs/.emacs.d/init.el  ~/.emacs.d/init.el
+cp ~/repo/archBak/config/tmux/.tmux.conf         ~/.tmux.conf
+cp ~/repo/archBak/config/xpdf/.xpdfrc            ~/.xpdfrc
+cp ~/repo/archBak/config/conky/.conkyrc          ~/.conkyrc
+sudo cp ~/repo/archBak/config/vim/vimrc           /etc/vimrc
