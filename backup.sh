9653b9d1e243ca8ae00d0e6f591e7b27de8a1c25
Stanley Miracle <logicomiracle@gmail.com>
2015-11-16 01:28:46 -0800

    UPDATE

--- backup.sh
+++ backup.sh
@@ -1,15 +1,16 @@
-cp ~/.config/awesome/rc.lua ~/repo/archBak/config/awesome/rc.lua
-cp ~/.config/awesome/sound.sh ~/repo/archBak/config/awesome/sound.sh
-cp ~/.config/awesome/ones.sh ~/repo/archBak/config/awesome/ones.sh
-cp ~/.config/awesome/dual.sh ~/repo/archBak/config/awesome/dual.sh
-cp ~/.bashrc ~/repo/archBak/config/shell/.bashrc
-cp ~/.xinitrc ~/repo/archBak/config/shell/.xinitrc
-cp ~/.Xresources ~/repo/archBak/config/shell/.Xresources
-cp ~/.bash_profile ~/repo/archBak/config/shell/.bash_profile
-cp ~/.emacs.d/init.el ~/repo/archBak/config/emacs/.emacs.d/init.el
-cp ~/.tmux.conf ~/repo/archBak/config/tmux/.tmux.conf
-cp /etc/vimrc ~/repo/archBak/config/vim/vimrc
-cp ~/.xpdfrc ~/repo/archBak/config/xpdf/.xpdfrc
-cp ~/.conkyrc ~/repo/archBak/config/conky/.conkyrc
+cp ~/.config/awesome/rc.lua    ~/repo/archBak/config/awesome/rc.lua
+cp ~/.config/awesome/sound.sh  ~/repo/archBak/config/awesome/sound.sh
+cp ~/.config/awesome/ones.sh   ~/repo/archBak/config/awesome/ones.sh
+cp ~/.config/awesome/dual.sh   ~/repo/archBak/config/awesome/dual.sh
+cp ~/.bashrc                   ~/repo/archBak/config/shell/.bashrc
+cp ~/.xinitrc                  ~/repo/archBak/config/shell/.xinitrc
+cp ~/.Xresources               ~/repo/archBak/config/shell/.Xresources
+cp ~/.bash_profile             ~/repo/archBak/config/shell/.bash_profile
+cp ~/.emacs.d/init.el          ~/repo/archBak/config/emacs/.emacs.d/init.el
+cp ~/.tmux.conf                ~/repo/archBak/config/tmux/.tmux.conf
+cp ~/.xpdfrc                   ~/repo/archBak/config/xpdf/.xpdfrc
+cp ~/.conkyrc                  ~/repo/archBak/config/conky/.conkyrc
+cp  /etc/vimrc                 ~/repo/archBak/config/vim/vimrc
+
 pacman -Qqen > ~/repo/archBak/pkglist.txt
 pacman -Qemq > ~/repo/archBak/aurlist.txt
